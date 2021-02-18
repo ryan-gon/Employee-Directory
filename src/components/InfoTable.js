44513c82ef0003a895c012f331e5b8dd25e15443
Ryan Matthew Gonzalez <ryanpiunj@gmail.com>
2021-02-18 10:35:20 -0500

    intial commit

---
+++ src/components/InfoTable.js
@@ -0,0 +1,38 @@
+import React from "react";
+import InfoBody from "./InfoBody";
+import "../styles/InfoTable.css";
+
+function InfoTable({ headings, users, handleSort }) {
+  return (
+    <div className="datatable mt-5">
+      <table
+        id="table"
+        className="table table-striped table-hover table-condensed"
+      >
+        <thead>
+          <tr>
+            {headings.map(({ name, width }) => {
+              return (
+                <th
+                  className="col"
+                  key={name}
+                  style={{ width }}
+                  onClick={() => {
+                    handleSort(name.toLowerCase());
+                  }}
+                >
+                  {name}
+                  <span className="pointer"></span>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+
+        <InfoBody users={users} />
+      </table>
+    </div>
+  );
+}
+
+export default InfoTable;
